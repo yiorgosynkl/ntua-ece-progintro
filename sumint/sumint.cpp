32afa121c4d05003b710fe86b85ad8d81b4e464f
yiorgosynkl <yiorgosynkl@gmail.com>
2020-01-29 11:25:23 +0200

    sumint done

---
+++ sumint/sumint.cpp
@@ -0,0 +1,47 @@
+/**********************************************************************
+* Project           : ntua-ece-progintro
+* Program name      : sumint.cpp
+* Author            : yiorgosynkl (find me in Github: https://github.com/yiorgosynkl)
+* Date created      : 20200129
+* Purpose           : subarrays with sum less than K
+**********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    long n_nums, limit;
+    cin >> n_nums; cin >> limit;
+
+    long *nums = new long[n_nums];
+    for (long i = 0; i < n_nums; i++)
+        cin >> nums[i];
+
+    long left_idx = 0;
+    long right_idx = 0;
+    long long sum = nums[0];
+    long long res = 0; 
+
+    for (;;) {
+        // cout << "left: " << left_idx << ", right: " << right_idx << ", sum: " << sum << " ==> res : ";
+        if (sum <= limit){
+            res += right_idx - left_idx + 1; // the amount of subarrays with sum less than the limit (removing nums can only reduce the sum)
+            right_idx++;
+            if (right_idx >= n_nums) // terminating condition
+                break;
+            sum += nums[right_idx];
+        }
+        else{
+            sum -= nums[left_idx];
+            left_idx++;
+            if (left_idx > right_idx)
+                right_idx++;
+        }
+        // cout << res << endl;
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
